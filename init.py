797c16d8f4b42ab860bc8900bc489ea5d8b183ea
dennisYatunin <dyatun@gmail.com>
2016-01-07 13:20:57 -0500

    weee more stuff

--- init.py
+++ init.py
@@ -1,12 +1,35 @@
 import sqlite3
 
 print("HALLOA! Welcome to the Icarus initialization stuffs. Please answer some questions for us.")
-maxcourses = int(input('How many periods are there in a day? '))
-nummesters = int(input('Do you have 2 mesters (semester system) or 3 mesters (trimester system) or something else? '))
+maxcourses = int(raw_input('How many periods are there in a day? '))
+nummesters = int(raw_input('Do you have 2 mesters (semester system) or 3 mesters (trimester system) or something else? '))
 
 conn = sqlite3.connect('data.db')
+# NEED TO FIX ADDRESSES
 
-# add student stuffs
 c = conn.cursor()
 q = 'CREATE TABLE students \
-	(id TEXT, username TEXT, offname TEXT, salt INT, hash_value INT, address TEXT, email TEXT)'
+	( \
+		id TEXT, username TEXT, offname TEXT, salt INT, hash_value INT, \
+		dob TEXT, address TEXT, city TEXT, zip TEXT, email TEXT, phone TEXT, \
+		cursched TEXT, pastscheds TEXT \
+		)'
+c.execute(q)
+
+q = 'CREATE TABLE parents \
+	( \
+		id TEXT, studid TEXT \
+		)'
+c.execute(q)
+
+q = 'CREATE TABLE facutly \
+	( \
+		id TEXT, username TEXT, offname TEXT, salt INT, hash_value INT, permissions INT\
+		dob TEXT, address TEXT, city TEXT, zip TEXT, email TEXT, phone TEXT, \
+		cursched TEXT, pastscheds TEXT \
+		)'
+c.execute(q)
+
+keepadding = raw_input('Do you have some data to import? (y/n) ')
+if keepadding == 'y':
+	open('../data.csv')
