7571d83c0e1cb6161a5ea04ed2489a72ec79990c
dennisYatunin <dyatun@gmail.com>
2016-01-08 13:20:35 -0500

    YAAAASSSS

--- init.py
+++ init.py
@@ -10,29 +10,36 @@ conn = sqlite3.connect('data.db')
 c = conn.cursor()
 q = 'CREATE TABLE students \
 	( \
-		id TEXT, username TEXT, offname TEXT, salt INT, hash_value INT, \
-		dob TEXT, address TEXT, city TEXT, zip TEXT, email TEXT, phone TEXT, \
+		id TEXT, email TEXT, name TEXT, salt INT, hash_value INT, \
+		dob TEXT, address TEXT, city TEXT, zip TEXT, phone TEXT, \
 		cursched TEXT, pastscheds TEXT \
 		)'
 c.execute(q)
 
 q = 'CREATE TABLE parents \
 	( \
-		id TEXT, studid TEXT \
+		id TEXT, email TEXT, name TEXT, salt INT, hash_value INT, \
+		studids TEXT, address TEXT, city TEXT, zip TEXT, phone TEXT \
 		)'
 c.execute(q)
 
 q = 'CREATE TABLE facutly \
 	( \
-		id TEXT, username TEXT, offname TEXT, salt INT, hash_value INT, permissions INT\
-		dob TEXT, address TEXT, city TEXT, zip TEXT, email TEXT, phone TEXT, \
+		id TEXT, email TEXT, name TEXT, salt INT, hash_value INT, permissions INT\
+		dob TEXT, address TEXT, city TEXT, zip TEXT, phone TEXT, \
 		cursched TEXT, pastscheds TEXT \
 		)'
 c.execute(q)
 
-keepadding = raw_input('Do you have some data to import? (y/n) ')
-if keepadding == 'y':
-	open('../data.csv')
+q = []
+students = open('../data/students.csv').readlines()
+for line in students:
+	line = line.split(',')
+	q.append('INSERT ')
+
+q = ['INSERT INTO students (id, email, name, salt, hash_value, dob, address, city, zip, phone) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)' % (line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]) for line in open('../data/students.csv', 'r').readlines()]
+open('../data/parents.csv')
+open('../data/faculty.csv')
 
 
 #when importing data, csv is same as query, in order EXCEPT
